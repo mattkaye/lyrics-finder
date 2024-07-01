6208bb54441771960e48cd8f671aeb72c683636d
Matthew Kaye <mattkaye79@gmail.com>
2024-07-01 15:23:38 -0400

    Adding modal. Adding Interpret component.

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ function App() {
             <span className="text-4xl">{lyricData.error} ☹️</span>
           )}
         </section>
-        <InterpretLyrics />
+        <InterpretLyrics lyrics={lyricData.lyrics} />
       </main>
     </div>
   );
--- src/components/MagicBall/InterpretLyrics.tsx
+++ src/components/MagicBall/InterpretLyrics.tsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
 import { cn } from "../../utils/cn";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
 import "./styles.css";
 
-export const InterpretLyrics = () => {
+export const InterpretLyrics = ({ lyrics }: { lyrics: string }) => {
   const [open, setOpen] = useState(false);
+  const [interpretation, setInterpretation] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+    setInterpretation(false);
+  };
 
   const toggleMagicBox = () => {
     setOpen(!open);
   };
+
+  const handleInterpretLyricsCall = async () => {
+    setOpenModal(true);
+    const response = await fetch("https://fakeresponder.com/?sleep=2000");
+
+    const data = await response.json();
+    setInterpretation(data.about);
+    console.log(data);
+  };
+
+  // Remove the line breaks
+  const cleanLyrics = lyrics.replaceAll("\n", " ");
 
   return (
     <aside className={cn("magic-ball-wrapper", open ? "active" : "")}>
@@ -17,7 +38,20 @@ export const InterpretLyrics = () => {
           🔮
         </button>
       </div>
-      <button className="interpret">Interpret Lyrics</button>
+      <button className="interpret" onClick={handleInterpretLyricsCall}>
+        Interpret Lyrics
+      </button>
+      <Modal
+        classNames={{
+          modal: "p-20 text-black",
+        }}
+        open={openModal}
+        onClose={onCloseModal}
+        center
+      >
+        {!interpretation && <h1 className="text-3xl">Thinking... 🤔</h1>}
+        {interpretation && interpretation}
+      </Modal>
     </aside>
   );
 };
