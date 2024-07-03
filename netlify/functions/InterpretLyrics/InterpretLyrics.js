545fa237567170e214dd678f11fed548355be168
Matthew Kaye <mattkaye79@gmail.com>
2024-07-02 21:57:52 -0400

    Dropping Gemini's sensitivity to lyrics.

--- netlify/functions/InterpretLyrics/InterpretLyrics.js
+++ netlify/functions/InterpretLyrics/InterpretLyrics.js
@@ -1,17 +1,42 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
 export const handler = async (event) => {
   const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-  const model = genAI.getGenerativeModel(
-    { model: process.env.GEMINI_MODEL_NAME },
-    { apiVersion: "v1beta" }
-  );
-  const prompt = `Interpret the meaning of these lyrics: ${event.body}`;
 
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+  ];
+
+  const model = genAI.getGenerativeModel({
+    model: process.env.GEMINI_MODEL_NAME,
+    safetySettings,
+  });
+
+  const prompt = `Interpret the meaning of these lyrics: ${event.body}`;
   const result = await model.generateContent(prompt);
 
   try {
     const response = result.response;
+    console.log(response);
     const interpretation = response.text();
     return {
       statusCode: 200,
